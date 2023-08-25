aa77ef4f281d5a5089304d0c8129dcd5db58c525
jiho <dream_cacao_jh@naver.com>
2023-08-25 19:51:10 +0900

    add check parking areas beta

--- src/System.cc
+++ src/System.cc
@@ -37,7 +37,7 @@ bool has_suffix(const std::string &str, const std::string &suffix) {
 namespace ORB_SLAM2
 {
 
-System::System(const string &strVocFile, const string &strSettingsFile, const string &strSequencePath,
+System::System(const string &strVocFile, const string &strSettingsFile, const string &strSequencePath, const string &strParkingAreas,
                const eSensor sensor) :
                mSensor(sensor), mpViewer(static_cast<Viewer*>(NULL)),
                mbReset(false),mbActivateLocalizationMode(false), mbDeactivateLocalizationMode(false)
