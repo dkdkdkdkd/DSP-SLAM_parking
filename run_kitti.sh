b0a16e3700b34d96f62e1bedd65c7e4949766c00
jiho <dream_cacao_jh@naver.com>
2023-10-17 20:56:42 +0900

    add parking jone

--- run_kitti.sh
+++ run_kitti.sh
@@ -2,3 +2,4 @@ cd build
 make -j20
 cd ..
 ./dsp_slam Vocabulary/ORBvoc.bin configs/KITTI04-12.yaml data/kitti/07 map/07 0
+# ./dsp_slam Vocabulary/ORBvoc.bin configs/KITTI04-12.yaml data/half_dataset/l map/m 0
--- src/ObjectDrawer.cc
+++ src/ObjectDrawer.cc
@@ -17,6 +17,48 @@
 
 #include "ObjectDrawer.h"
 
+struct Point
+{
+    double x, y;
+    Point(double x, double y) : x(x), y(y) {}
+};
+
+// 직사각형 구조체 정의
+struct Rectangle
+{
+    Point A, B, C, D;
+    Rectangle(Point A, Point B, Point C, Point D) : A(A), B(B), C(C), D(D) {}
+};
+
+bool isPointInsideRectangle(const Rectangle &rect, const Point &P)
+{
+    Eigen::Vector3d AB(rect.B.x - rect.A.x, rect.B.y - rect.A.y, 0);
+    Eigen::Vector3d BC(rect.C.x - rect.B.x, rect.C.y - rect.B.y, 0);
+    Eigen::Vector3d CD(rect.D.x - rect.C.x, rect.D.y - rect.C.y, 0);
+    Eigen::Vector3d DA(rect.A.x - rect.D.x, rect.A.y - rect.D.y, 0);
+
+    Eigen::Vector3d PA(rect.A.x - P.x, rect.A.y - P.y, 0);
+    Eigen::Vector3d PB(rect.B.x - P.x, rect.B.y - P.y, 0);
+    Eigen::Vector3d PC(rect.C.x - P.x, rect.C.y - P.y, 0);
+    Eigen::Vector3d PD(rect.D.x - P.x, rect.D.y - P.y, 0);
+
+    // 사각형(삼각형 2개의 합) 넓이
+    double areaABD = 0.5 * AB.cross(-DA).norm();
+    double areaBCD = 0.5 * CD.cross(-BC).norm();
+    double totalArea = areaABD + areaBCD;
+
+    // 4개의 삼각형 넓이
+    double totalAreaFourTriangles = 0;
+    totalAreaFourTriangles += 0.5 * PA.cross(PB).norm();
+    totalAreaFourTriangles += 0.5 * PB.cross(PC).norm();
+    totalAreaFourTriangles += 0.5 * PC.cross(PD).norm();
+    totalAreaFourTriangles += 0.5 * PD.cross(PA).norm();
+
+    // 오차 인정 범위
+    double error = 6;
+    return totalArea + error >= totalAreaFourTriangles && totalAreaFourTriangles >= totalArea - error;
+}
+
 namespace ORB_SLAM2
 {
 
@@ -63,74 +105,74 @@ void ObjectDrawer::ProcessNewObjects()
 
 void ObjectDrawer::DrawObjects(bool bFollow, const Eigen::Matrix4f &Tec)
 {
-    unique_lock<mutex> lock(mMutexObjects);
+        unique_lock<mutex> lock(mMutexObjects);
 
-    auto mvpMapObjects = mpMap->GetAllMapObjects();
-    //   add check parking area     
-    ifstream file("/home/jiho/slam/DSP-SLAM_parking/parking_areas.txt");
+        auto mvpMapObjects = mpMap->GetAllMapObjects();
 
-    string line;
-    string tmp;
-    int count = 0;
-    float distance;
-    vector <vector<float>> areas;
-    
-
-    if(file.is_open()){
-        while(getline(file, line)) {
-            // if (count%2==1){
-            vector<float> area;
-            stringstream ss(line);
-            while(getline(ss, tmp, ' ')){
-                // cout << tmp << endl;
-                area.push_back(std::stof(tmp));
-            }
-                areas.push_back(area);
-                // }
-            count++;
-        }
-    }
-
-    for (MapObject *pMO : mvpMapObjects)
-    {
-        if (!pMO)
-            continue;
-        if (pMO->isBad())
-            continue;
-
-        Eigen::Matrix4f Sim3Two = pMO->GetPoseSim3();
-
-        for(auto iter:areas){
-            distance = 0.0;
-
-                distance += (Sim3Two(0, 3) - iter[0*4+3])*(Sim3Two(0, 3) - iter[0*4+3]);
-                distance += (Sim3Two(2, 3) - iter[2*4+3])*(Sim3Two(2, 3) - iter[2*4+3]);
-            
-            distance = sqrt(distance);
-            if (distance<5){
-                pMO->checkArea = 1;
-                // cout<<"id: " << pMO->mnId << " " << "True" << endl;
-                break;
-            }
-            else
-                pMO->checkArea = 0;
-        }
-        cout <<pMO->checkArea<<endl;
-
-
-        int idx = pMO->GetRenderId();
-
-        if (bFollow) {
-            SE3TcwFollow = SE3Tcw;
-        }
-        if (pMO->GetRenderId() >= 0)
+        for (MapObject *pMO : mvpMapObjects)
         {
-            // mpRenderer->Render(idx, Tec * SE3TcwFollow * Sim3Two, mvObjectColors[pMO->GetRenderId() % mvObjectColors.size()]);
-            mpRenderer->Render(idx, Tec * SE3TcwFollow * Sim3Two, mvObjectColors[pMO->checkArea]);
+            if (!pMO)
+                continue;
+            if (pMO->isBad())
+                continue;
+
+            Eigen::Matrix4f Sim3Two = pMO->GetPoseSim3();
+            int idx = pMO->GetRenderId();
+
+            if (bFollow)
+            {
+                SE3TcwFollow = SE3Tcw;
+            }
+            if (pMO->GetRenderId() >= 0)
+            {
+
+                std::ifstream file("/home/jiho/slam/DSP-SLAM_parking/data/legal_parking_zone_coordinates.txt");
+                std::vector<Rectangle> rectangles;
+                std::string line;
+                while (std::getline(file, line))
+                {
+                    std::istringstream iss(line);
+                    double x1, y1, x2, y2, x3, y3, x4, y4;
+                    if (iss >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4)
+                    {
+                        Point lt(x1, y1);
+                        Point lb(x2, y2);
+                        Point rb(x3, y3);
+                        Point rt(x4, y4);
+                        Rectangle rect(lt, lb, rb, rt);
+                        rectangles.push_back(rect);
+                    }
+                    else
+                    {
+                        std::cerr << "잘못된 입력 형식입니다: " << line << std::endl;
+                    }
+                }
+
+                file.close();
+
+                double m, n;
+                m = Sim3Two(0, 3);
+                n = Sim3Two(2, 3);
+
+                int color = 0;
+                for (size_t i = 0; i < rectangles.size(); ++i)
+                {
+                    if (isPointInsideRectangle(rectangles[i], Point(m, n)))
+                    {
+                        color = 1;
+                        pMO->checkArea = 1;
+                        break;
+                    }
+                }
+
+                cout << m << ", " << n << "에 위치한 차량은 " << ((color == 0) ? "불법" : "합법") << "차량입니다" << endl;
+
+                // mpRenderer->Render(idx, Tec * SE3TcwFollow * Sim3Two, mvObjectColors[pMO->GetRenderId() % mvObjectColors.size()]);
+                mpRenderer->Render(idx, Tec * SE3TcwFollow * Sim3Two, mvObjectColors[color]);
+            }
+            // DrawCuboid(pMO);
         }
-        // DrawCuboid(pMO);
     }
-}
 
 void ObjectDrawer::DrawCuboid(MapObject *pMO)
 {
